29249aab15bb9cd37822aa1f13dd51ce992544b7
StevenMHernandez <me@stevenmhernandez.com>
2020-10-22 16:36:48 -0400

    hw3: initial commit

---
+++ hw3/solution.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: hernandezsm
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy as sp
+import tensorflow as tf;
+
+#for reproducibility between runs
+np.random.seed(123)
