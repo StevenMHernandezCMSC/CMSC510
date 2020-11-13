5f400eac6288eb3f734c8188fd72065cbb641402
StevenMHernandez <me@stevenmhernandez.com>
2020-11-13 12:28:29 -0500

    hw4: add command line parameters to set model parameters for tensorflow, update formatting for both pytorch and tensorflow scripts

--- hw4/a.pytorch.py
+++ hw4/a.pytorch.py
@@ -7,131 +7,140 @@ import torchvision.datasets as datasets
 
 
 def run(NUM_CNN_LAYERS, CNN_FILTER_SIZE):
-  #
-  # CONFIG
-  #
-  print("NUM_CNN_LAYERS =", NUM_CNN_LAYERS) # [0,1,2]
-  print("CNN_FILTER_SIZE =", CNN_FILTER_SIZE) # [0,1,2]
+    #
+    # CONFIG
+    #
+    print("NUM_CNN_LAYERS =", NUM_CNN_LAYERS)
+    print("CNN_FILTER_SIZE =", CNN_FILTER_SIZE)
 
-  start_time = time()
-
-  # 
-  # Load MNIST Dataset
-  # 
-  mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
-  mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
-
-  # max_elements = 1000
-  max_elements = 1000000  # i.e. use all of them.
-
-  X_train = mnist_trainset.data[:max_elements]
-  y_train = mnist_trainset.targets[:max_elements]
-  X_test = mnist_testset.data[:max_elements]
-  y_test = mnist_testset.targets[:max_elements]
-
-  X_train = torch.reshape(X_train, (len(X_train), 1, 28, 28)).float()
-  X_test = torch.reshape(X_test, (len(X_test), 1, 28, 28)).float()
-
-  w = {
-    "0,0": 28*28,
-    "1,3": 169,
-    "1,5": 144,
-    "1,9": 100,
-    "2,3": 144,
-    "2,5": 100,
-    "2,9": 36,
-  }[f"{NUM_CNN_LAYERS},{CNN_FILTER_SIZE}"]
-
-  layers = []
-
-  for i in range(NUM_CNN_LAYERS):
-    layers.append(nn.Conv2d(1,1,CNN_FILTER_SIZE))
-    layers.append(nn.ReLU())
-    layers.append(nn.BatchNorm2d(1))
-
-  if NUM_CNN_LAYERS > 0:
-    layers.append(nn.MaxPool2d(2))
-
-  layers = layers + [
-    nn.ReLU(),
-    nn.Flatten(),
-
-    nn.Linear(w,10),
-    nn.ReLU(),
-  ]
-
-  model = nn.Sequential(*layers)
-
-  loss = nn.CrossEntropyLoss()
-
-  optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-
-  l_loss = []
-  l_acc = []
-  l_val_loss = []
-  l_val_acc = []
-
-  batch_size = 2048
-
-  for i in range(100):
-    print("Iteration:", i)
-    for j in range(0,len(X_train),batch_size):
-      jS=j;jE=min(len(X_train),j+batch_size)
-      X_batch=X_train[jS:jE,:]
-      y_batch=y_train[jS:jE]
-
-      # clear previous gradient calculations
-      optimizer.zero_grad()
-
-      # calculate f based on current value
-      y_pred = model(X_batch)
-      y_true = y_batch
-      z = loss(y_pred, y_true)
-
-      # calculate gradient of f w.r.t. w
-      z.backward()
-
-      # use the gradient to change w
-      optimizer.step()
+    start_time = time()
 
     #
-    # Calculate Training Error
+    # Load MNIST Dataset
     #
-    # calculate f based on current value
-    y_pred = model(X_train)
-    y_true = y_train
-    z = loss(y_pred, y_true)
-    accuracy = sum((y_pred.argmax(axis=1) - y_true).abs() == 0) / float(len(y_train))
-    l_loss.append(float(z))
-    l_acc.append(float(accuracy))
-    print("Training:  ", "loss:", float(z), "accuracy:", float(accuracy))
+    mnist_trainset = datasets.MNIST(root='./data', train=True, download=True, transform=None)
+    mnist_testset = datasets.MNIST(root='./data', train=False, download=True, transform=None)
+
+    # max_elements = 1000
+    max_elements = 1000000  # i.e. use all of them.
+
+    X_train = mnist_trainset.data[:max_elements]
+    y_train = mnist_trainset.targets[:max_elements]
+    X_test = mnist_testset.data[:max_elements]
+    y_test = mnist_testset.targets[:max_elements]
+
+    X_train = torch.reshape(X_train, (len(X_train), 1, 28, 28)).float()
+    X_test = torch.reshape(X_test, (len(X_test), 1, 28, 28)).float()
 
     #
-    # Calculate Testing Error
+    # Create Model
     #
-    y_pred = model(X_test)
-    y_true = y_test
-    z = loss(y_pred, y_true)
-    accuracy = sum((y_pred.argmax(axis=1) - y_true).abs() == 0) / float(len(y_test))
-    l_val_loss.append(float(z))
-    l_val_acc.append(float(accuracy))
-    print("Validation:", "loss:", float(z), "accuracy:", float(accuracy))
+    w = {
+        "0,0": 28 * 28,
+        "1,3": 169,
+        "1,5": 144,
+        "1,9": 100,
+        "2,3": 144,
+        "2,5": 100,
+        "2,9": 36,
+    }[f"{NUM_CNN_LAYERS},{CNN_FILTER_SIZE}"]
 
+    layers = []
 
-  print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    for i in range(NUM_CNN_LAYERS):
+        layers.append(nn.Conv2d(1, 1, CNN_FILTER_SIZE))
+        layers.append(nn.ReLU())
+        layers.append(nn.BatchNorm2d(1))
 
-  print("NUM_CNN_LAYERS =", NUM_CNN_LAYERS)
-  print("CNN_FILTER_SIZE =", CNN_FILTER_SIZE)
-  print("time_taken =", time() - start_time)
-  print("loss =", l_loss)
-  print("validation_loss =", l_val_loss)
-  print("acc =", l_acc)
-  print("validation_acc =", l_val_acc)
+    if NUM_CNN_LAYERS > 0:
+        layers.append(nn.MaxPool2d(2))
+
+    layers = layers + [
+        nn.ReLU(),
+        nn.Flatten(),
+
+        nn.Linear(w, 10),
+        nn.ReLU(),
+    ]
+
+    model = nn.Sequential(*layers)
+
+    loss = nn.CrossEntropyLoss()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+    #
+    # Train and evaluate model
+    #
+    l_loss = []
+    l_acc = []
+    l_val_loss = []
+    l_val_acc = []
+
+    batch_size = 2048
+    for i in range(100):
+        print("Iteration:", i)
+        for j in range(0, len(X_train), batch_size):
+            jS = j
+            jE = min(len(X_train), j + batch_size)
+            X_batch = X_train[jS:jE, :]
+            y_batch = y_train[jS:jE]
+
+            # clear previous gradient calculations
+            optimizer.zero_grad()
+
+            # calculate f based on current value
+            y_pred = model(X_batch)
+            y_true = y_batch
+            z = loss(y_pred, y_true)
+
+            # calculate gradient of f w.r.t. w
+            z.backward()
+
+            # use the gradient to change w
+            optimizer.step()
+
+            #
+            # Calculate Training Error
+            #
+            # calculate f based on current value
+        y_pred = model(X_train)
+        y_true = y_train
+        z = loss(y_pred, y_true)
+        accuracy = sum((y_pred.argmax(axis=1) - y_true).abs() == 0) / float(len(y_train))
+        l_loss.append(float(z))
+        l_acc.append(float(accuracy))
+        print("Training:  ", "loss:", float(z), "accuracy:", float(accuracy))
+
+        #
+        # Calculate Testing Error
+        #
+        y_pred = model(X_test)
+        y_true = y_test
+        z = loss(y_pred, y_true)
+        accuracy = sum((y_pred.argmax(axis=1) - y_true).abs() == 0) / float(len(y_test))
+        l_val_loss.append(float(z))
+        l_val_acc.append(float(accuracy))
+        print("Validation:", "loss:", float(z), "accuracy:", float(accuracy))
+
+    #
+    # Output Results for plotting
+    #
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+    print("NUM_CNN_LAYERS =", NUM_CNN_LAYERS)
+    print("CNN_FILTER_SIZE =", CNN_FILTER_SIZE)
+    print("time_taken =", time() - start_time)
+    print("loss =", l_loss)
+    print("validation_loss =", l_val_loss)
+    print("acc =", l_acc)
+    print("validation_acc =", l_val_acc)
+
 
 if __name__ == "__main__":
-  import sys
+    import sys
 
-  NUM_CNN_LAYERS = int(sys.argv[1]) if len(sys.argv) > 1 else 0
-  CNN_FILTER_SIZE = int(sys.argv[2]) if len(sys.argv) > 2 else 0
+    NUM_CNN_LAYERS = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+    CNN_FILTER_SIZE = int(sys.argv[2]) if len(sys.argv) > 2 else 0
 
-  run(NUM_CNN_LAYERS, CNN_FILTER_SIZE)
+    run(NUM_CNN_LAYERS, CNN_FILTER_SIZE)
--- hw4/b.tensorflow.py
+++ hw4/b.tensorflow.py
@@ -1,34 +1,58 @@
-import numpy as np
-import tensorflow as tf
+from time import time
 from tensorflow import keras
 from tensorflow.keras import layers
 
-def run():
+def run(NUM_CNN_LAYERS, CNN_FILTER_SIZE):
+    #
+    # CONFIG
+    #
+    print("NUM_CNN_LAYERS =", NUM_CNN_LAYERS)
+    print("CNN_FILTER_SIZE =", CNN_FILTER_SIZE)
+
+    start_time = time()
+
+    #
+    # Build Model
+    #
     inputs = keras.Input(shape=(28,28,1))
-    x = layers.Conv2D(3, 3, input_shape=(28,28,1))(inputs)
-    x = layers.BatchNormalization()(x)
-    x = layers.Dense(64, activation='relu')(x)
-    x = layers.Conv2D(3, 3)(x)
-    x = layers.Dense(64, activation='relu')(x)
-    x = layers.MaxPooling2D()(x)
+    x = inputs
+
+    for i in range(NUM_CNN_LAYERS):
+        if i == 0:
+            x = layers.Conv2D(CNN_FILTER_SIZE, CNN_FILTER_SIZE, input_shape=(28,28,1))(x)
+        else:
+            x = layers.Conv2D(CNN_FILTER_SIZE, CNN_FILTER_SIZE)(x)
+        x = layers.Dense(64, activation='relu')(x)
+        x = layers.BatchNormalization()(x)
+
+    if NUM_CNN_LAYERS > 0:
+        x = layers.MaxPooling2D()(x)
     x = layers.Flatten()(x)
+
     x = layers.Dense(64, activation="relu")(x)
     outputs = layers.Dense(10)(x)
 
     model = keras.Model(inputs=inputs, outputs=outputs, name="mnist_model")
 
-    keras.utils.plot_model(model, "model.png", show_shapes=True)
+    model.compile(
+        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        optimizer=keras.optimizers.SGD(learning_rate=0.01),
+        metrics=["accuracy"],
+    )
 
+    #
+    # Setup Dataset
+    #
     (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
     x_train = x_train.astype(dtype='float32')
     x_test = x_test.astype(dtype='float32')
     y_train = y_train.astype(dtype='float32')
     y_test = y_test.astype(dtype='float32')
 
-    x_train = x_train.reshape(len(x_train), 28, 28, 1).astype("float32") / 255
-    x_test = x_test.reshape(len(x_test), 28, 28, 1).astype("float32") / 255
+    x_train = x_train.reshape(len(x_train), 28, 28, 1)
+    x_test = x_test.reshape(len(x_test), 28, 28, 1)
 
-    # max_elements = 1000
+    # max_elements = 100
     max_elements = 1000000  # a.k.a. use all of them.
 
     x_train = x_train[:max_elements]
@@ -36,17 +60,33 @@ def run():
     x_test = x_test[:max_elements]
     y_test = y_test[:max_elements]
 
-    model.compile(
-        loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        optimizer=keras.optimizers.RMSprop(),
-        metrics=["accuracy"],
-    )
+    #
+    # Train and Evaluate
+    #
+    history = model.fit(x_train, y_train, batch_size=2048, epochs=10, validation_data=(x_test, y_test))
+    print("history", history.history)
 
-    history = model.fit(x_train, y_train, batch_size=64, epochs=20, validation_split=0.2)
+    results = model.evaluate(x_test, y_test, verbose=2)
+    print("Validation loss:", results[0])
+    print("Validation accuracy:", results[1])
 
-    test_scores = model.evaluate(x_test, y_test, verbose=2)
-    print("Test loss:", test_scores[0])
-    print("Test accuracy:", test_scores[1])
+    #
+    # Output Results for plotting
+    #
+    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+
+    print("NUM_CNN_LAYERS =", NUM_CNN_LAYERS)
+    print("CNN_FILTER_SIZE =", CNN_FILTER_SIZE)
+    print("time_taken =", time() - start_time)
+    print("loss =", history.history['loss'])
+    print("validation_loss =", history.history['val_loss'])
+    print("acc =", history.history['accuracy'])
+    print("validation_acc =", history.history['val_accuracy'])
 
 if __name__ == "__main__":
-    run()
+  import sys
+
+  NUM_CNN_LAYERS = int(sys.argv[1]) if len(sys.argv) > 1 else 0
+  CNN_FILTER_SIZE = int(sys.argv[2]) if len(sys.argv) > 2 else 0
+
+  run(NUM_CNN_LAYERS, CNN_FILTER_SIZE)
